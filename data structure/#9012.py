7d2c1c2d2211a9fb860f518161cfaeecf4d03188
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-05-03 16:45:50 +0900

    Update #9012.py

--- data structure/#9012.py
+++ data structure/#9012.py
@@ -1,2 +1,26 @@
 #9012
-#
+#https://www.acmicpc.net/problem/9012
+
+num = int(input())
+strings = []
+for _ in range(num):
+    st = input()
+    strings.append(st)
+
+for row in strings:
+    stack = []
+
+    for p in row:
+        if p=='(':
+            stack.append(p)
+        elif p==')':
+            if stack:
+                stack.pop()
+            else:
+                print('NO')
+                break
+    else:
+        if not stack:
+            print('YES')
+        else:
+            print('NO')
