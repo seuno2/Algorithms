1e79a96b9534400817233868e5f20e3e7df0f49d
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-06-20 10:40:59 +0900

    Create 스택수열.py

---
+++ data structure/스택수열.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/1874
+
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+answer = []
+flag = 0
+cur = 1
+
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    while cur <= num:
+        stack.append(cur)
+        answer.append("+")
+        cur += 1
+    
+    if stack[-1] == num:
+        answer.append("-")
+        stack.pop()
+
+    else :
+        print("NO")
+        flag = 1
+        break
+
+if flag == 0:
+    for i in answer:
+        print(i)
