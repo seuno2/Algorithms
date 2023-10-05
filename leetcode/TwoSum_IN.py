9f4c3f19865bbcd33641289e93b830b6510956dd
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-10-05 20:34:36 +0900

    Update TwoSum_IN.py

--- leetcode/TwoSum_IN.py
+++ leetcode/TwoSum_IN.py
@@ -5,7 +5,8 @@ class Solution:
         for i, num in enumerate(nums):
             complement = target - num
 
-          #i+1번째 이후의 요소들에서 검색(중복 피하기 위햬)
-          # in을 통한 탐색의 시간복잡도:O(n)
-            if complement in nums[i+1:]:                          
+            # i+1번째 이후의 요소들에서 검색(중복 피하기 위햬)
+            # in을 통한 탐색의 시간복잡도:O(n)
+            # 부루트 포스 탐색처럼 매번 비교하는 것보다 in 사용이 훨씬 빠르게 실행된다
+            if complement in nums[i+1:]:
                 return [i, nums[i+1:].index(complement) + (i+1)]
