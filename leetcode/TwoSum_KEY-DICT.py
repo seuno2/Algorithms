28dfb62569fa04c32e80d0b71a3d5c026d9ff95b
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-10-05 20:55:37 +0900

    Update TwoSum_KEY-DICT.py

--- leetcode/TwoSum_KEY-DICT.py
+++ leetcode/TwoSum_KEY-DICT.py
@@ -4,7 +4,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_map = {key:val for key,val in zip(nums, range(len(nums)))}
         
-        
+        # 딕셔너리는 해시맵으로 탐색 시간복잡도 : O(1)
+        # 전체 시간복잡도 : O(n)
         for i, n in enumerate(nums):
             if target - n in nums_map and i!=nums_map[target - n]:
                 return [i, nums_map[target - n]]
