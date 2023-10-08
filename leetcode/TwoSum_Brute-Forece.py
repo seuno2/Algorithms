c5e37b69b6df8f6b986557d4a3d4c8b0411b2f9b
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-10-08 20:30:56 +0900

    Delete leetcode/TwoSum_Brute-Forece.py

--- leetcode/TwoSum_Brute-Forece.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/two-sum/
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #이중for문으로 부루트 포스 탐색의 시간 복잡도 : O(n^2)
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
