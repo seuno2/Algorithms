c67a4a4b8c5fcdc574aa8f1ae4633b147ffbf95c
seuno2 <121914727+seuno2@users.noreply.github.com>
2023-10-08 23:06:09 +0900

    Update Stack.py

--- LeetCode/TrappingRainWater/Stack.py
+++ LeetCode/TrappingRainWater/Stack.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/trapping-rain-water/
+# https://leetcode.com/problems/trapping-rain-water/
 
 class Solution:
     def trap(self, height: List[int]) -> int:
@@ -6,16 +6,20 @@ class Solution:
         volume = 0
 
         for i in range(len(height)):
+            # 직전보다 벽이 높아지는 경우
             while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
+                # 직전의 높이를 offset으로 설정(나중에 물의 높이에서 빼기위해)
+                offset = height[stack.pop()]
 
                 if not len(stack):
                     break
                 
                 distance = i - stack[-1] - 1
-                waters = min(height[i], height[stack[-1]]) - height[top]
+                # 현재의 벽높이와 stack 직전 벽높이 중 낮은값까지 물이 차있음
+                waterHeight = min(height[i], height[stack[-1]]) - offset
 
-                volume += distance * waters
+                volume += distance * waterHeight
+                # print('+', distance * waterHeight)
 
             stack.append(i)
         return volume
